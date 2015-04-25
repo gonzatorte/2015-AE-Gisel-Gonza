4a7a9d5e97f89e2ed3cda3937a55e83d758967d7
gonzatorte <gonzatorte@gmail.com>
2015-04-24 22:21:09 -0300

    agrego varios ejemplos de problemas

--- tester.sh
+++ tester.sh
@@ -2,9 +2,9 @@
 
 for i in $(seq 1 20) ; do
 	./gen 100 50 1 $i
-	mv test.in test_${i}.in
+	mv test.in "problems/test_${i}.in"
 done
 
 for i in $(seq 1 20) ; do
-	./ejercicio2 "test_${i}.in"
+	./ejercicio2 "problems/test_${i}.in"
 done
