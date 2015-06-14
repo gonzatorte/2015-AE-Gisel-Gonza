5130308cc319054426441de3924c1426046f1d6d
gonzatorte <gonzatorte@gmail.com>
2015-06-14 19:47:50 -0300

    cosas de informe parte 1

--- Lab/informe/build.sh
+++ Lab/informe/build.sh
@@ -2,10 +2,10 @@
 
 # cat informe.tex | sed -n "s/[á]/\\\'a/ p" > informe.tex.tmp
 
-./clean.sh
-pdflatex informe.tex
-pdflatex informe.tex
-bibtex informe
-pdflatex informe.tex
-pdflatex informe.tex
+./clean.sh &&
+pdflatex informe.tex &&
+pdflatex informe.tex &&
+bibtex informe &&
+pdflatex informe.tex &&
+pdflatex informe.tex &&
 ./clean.sh
