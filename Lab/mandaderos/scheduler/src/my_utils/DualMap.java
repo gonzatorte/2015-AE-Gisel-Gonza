0f8c8499214044858e4bbb575b467514e5f7db8c
giselc <gisel_8@hotmail.com>
2015-08-18 21:07:31 -0300

    serializar
    serializar

--- Lab/mandaderos/scheduler/src/my_utils/DualMap.java
+++ Lab/mandaderos/scheduler/src/my_utils/DualMap.java
@@ -1,10 +1,11 @@
 package my_utils;
 
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.TreeMap;
 import java.util.HashMap;
 import java.util.Map;
 
 
-public class DualMap<K1, K2, V> extends HashMap<K1,HashMap<K2,V>>{
+public class DualMap<K1, K2, V> extends HashMap<K1,HashMap<K2,V>> implements Serializable {
 }
