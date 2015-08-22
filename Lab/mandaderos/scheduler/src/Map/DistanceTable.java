fb69666c90545411d7c3b00f21d4cede1ed00d38
gonzatorte <gonzatorte@gmail.com>
2015-08-22 10:32:59 -0300

    fixi

--- Lab/mandaderos/scheduler/src/Map/DistanceTable.java
+++ Lab/mandaderos/scheduler/src/Map/DistanceTable.java
@@ -8,16 +8,46 @@ import java.util.Set;
 
 public class DistanceTable extends HashMap<Place,HashMap<Place,Double>> implements Serializable {
     public void removePlace(Place p){
+        //Remover la row y de todas las demas filas que vienen despues y
+        //tienen el place
         this.remove(p);
         for (HashMap<Place,Double> distances : this.values()){
             distances.remove(p);
         }
     }
     
-    public void addPlace(Place p, HashMap<Place, Double> distances){
-        this.put(p, distances);
+    /*
+    Esto se dedica a solo dejar las distancias que interesan, hay que correrlo
+    Antes de ejecutar el AE, pero despues de aplicar los eventos
+    */
+    public void compact(List<Place> places){
+//        this.remove(p);
+//        for (HashMap<Place,Double> distances : this.values()){
+//            distances.remove(p);
+//        }
     }
     
+    public void addPlace(Place p1, HashMap<Place, Double> distances){
+        Set<Entry<Place, Double>> d_ps = distances.entrySet();
+        HashMap<Place, Double> new_row = new HashMap<Place, Double>();
+        for (Entry<Place, Double> d_p : d_ps){
+            Place p2 = d_p.getKey();
+            int cmp = p1.compareTo(p2);
+            if (cmp > 0){
+                this.get(p2).put(p1, d_p.getValue());
+            } else if (cmp < 0) {
+                new_row.put(p2, d_p.getValue());
+            } else {
+                throw new Error("El lugar ya existe");
+            }
+        }
+        this.put(p1, new_row);
+    }
+    
+    /*
+    Si no lo encuentra aqui, puede ser que vaya a buscarlo a la web
+    O a la BD de crawling (un SQLlite)
+    */
     public Double getDistance(Place p1, Place p2){
         int cmp = p1.compareTo(p2);
         if (cmp > 0){
